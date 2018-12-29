8637d1774260eb972078f1355b570ee8f3015673
Timothy W. Cook <tim@datainsights.tech>
2018-12-29 09:52:09 -0200

    Fix catalog and  dmlib dir locations. Fix RM location in catalog.

--- kunteksto/catalogs.py
+++ kunteksto/catalogs.py
@@ -5,14 +5,16 @@ def make_catalog(config):
     Create an XML Catalog based on the installation location.    
     """
     RMVERSION = config['SYSTEM']['rmversion'].replace('.', '_')
-    catdir = os.path.join(os.getcwd(), 'catalogs')
-    dmlib = os.path.join(os.getcwd(), 'dmlib')
+    parentdir = os.path.join(os.getcwd(), os.pardir)
+    catdir = os.path.join(parentdir, 'catalogs')
+    dmlib = os.path.join(parentdir, 'dmlib')
+    rmdir = os.path.join(parentdir, 's3model')
 
     xmlcat = '<?xml version="1.0" encoding="UTF-8"?>\n'
     xmlcat += '<!DOCTYPE catalog PUBLIC "-//OASIS//DTD XML Catalogs V1.1//EN" "http://www.oasis-open.org/committees/entity/release/1.1/catalog.dtd">'
     xmlcat += '<catalog xmlns="urn:oasis:names:tc:entity:xmlns:xml:catalog">\n'
     xmlcat += '  <!-- S3Model RM Schema -->\n'
-    xmlcat += '    <uri name="https://www.s3model.com/ns/s3m/s3model_' + RMVERSION + '.xsd" uri="../s3model/s3model_' + RMVERSION + '.xsd"/>\n'    
+    xmlcat += '    <uri name="https://www.s3model.com/ns/s3m/s3model_' + RMVERSION + '.xsd" uri="' + rmdir + '/s3model/s3model_' + RMVERSION + '.xsd"/>\n'
     xmlcat += '  <!-- S3Model DMs -->\n'    
     xmlcat += '    <rewriteSystem systemIdStartString="https://s3model.com/dmlib/" rewritePrefix="file:///' + dmlib + '/"/>\n'    
     xmlcat += '</catalog>\n'    
